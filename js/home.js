fd36978c25ee1a0f3a242e22bbc6288d78a5f876
Foysal <ahmedmdfoysal123@gmail.com>
2024-09-19 21:26:56 +0600

    added the input value js part

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-console.log('home added')
+// add money to the account
+
+
+// step 1: get button and add prevetDefault
+// step 2: get money amount from the input to add in balance
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // add money input
+    const addMoney = document.getElementById('input-add-money').value;
+    console.log(addMoney)
+
+    // get the provided pin number
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+
+})
