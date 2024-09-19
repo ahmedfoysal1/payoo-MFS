fd06036f680ad4433181bf9b6fe8d5be2463f6f7
Foysal <ahmedmdfoysal123@gmail.com>
2024-09-19 21:51:46 +0600

    added the money added funtion in js

--- js/home.js
+++ js/home.js
@@ -2,8 +2,12 @@
 
 
 // step 1: get button and add prevetDefault
-// step 2: get money amount from the input to add in balance
-document.getElementById('btn-add-money').addEventListener('click', function(event){
+// step 2: get money amount from the input to add in balance also get the pin number
+// step 3 verify the pin number 
+// step 4 get the current balance
+// step 5 add addMoneyInput with current balance
+// step 6 update the balance in UI/DOM 
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
     event.preventDefault();
 
     // add money input
@@ -13,4 +17,24 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // get the provided pin number
     const pinNumberInput = document.getElementById('input-pin-number').value;
 
+    // step 3 verify the pin number 
+    // this method only for learning purpose and for begginer
+    if (pinNumberInput === '12') {
+        console.log('money added successfully')
+        // step 4 get the current balance
+        const currentBalance = document.getElementById('balance').innerText;
+        console.log(currentBalance);
+        // step 5 add addMoneyInput with current balance
+        const addMoneyNumber = parseFloat(addMoney);
+        const balanceNumber = parseFloat(currentBalance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step 6 update the balance in UI/DOM 
+        document.getElementById('balance').innerText = newBalance;
+
+    }
+    else {
+        alert('Wrong Pin Number')
+    }
 })
